72e8c2a035535a4d2839a564442536d632e0428b
hsuyunyun <5b1g0011@stust.edu.tw>
2025-05-22 10:10:28 +0800

    更新 Vite 配置以設置正確的基底路徑

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // 使用者頁面要設成 '/'
+  base: '/my-react-app/',
   plugins: [react()],
 })
